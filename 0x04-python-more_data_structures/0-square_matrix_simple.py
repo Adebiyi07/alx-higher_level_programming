f2be36031197aa6984ba0bff2ba25fa5659e690f
Adebiyi07 <adebiyijohn07@gmail.com>
2024-01-10 20:53:49 -0800

    Task0 fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix to store squared values
     result_matrix = []
 
-    # Iterate over each row in the original matrix
     for row in matrix:
-        # Create a new row for the result matrix
         result_row = []
-        # Iterate over each element in the row and append its square to the result row
         for element in row:
             result_row.append(element ** 2)
-        # Append the result row to the result matrix
         result_matrix.append(result_row)
 
     return result_matrix
