c184435c10372bf4c1154f8a432a5ffa5d8e1108
Adebiyi07 <adebiyijohn07@gmail.com>
2024-01-23 13:36:53 -0800

    Task1 python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#1-square.py class
+"""Defines a square"""
+
+
+class Square:
+    """class to rep square"""
+
+    def __init__(self, size):
+        """Square class is being initialized"""
+
+        self.__size = size
